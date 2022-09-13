cf42af39b84b30fa0ac553c2c2fc22e6d04ca734
Evan-Rousseau-GTech <erousseau@gaming.tech>
2022-09-13 14:56:59 +0200

    Add Fan

--- NY-Roof-Runner/Assets/WorldGeneration/WorldGeneration.cs
+++ NY-Roof-Runner/Assets/WorldGeneration/WorldGeneration.cs
@@ -7,20 +7,33 @@ public class WorldGeneration : MonoBehaviour
     public GameObject floorObject;
     public GameObject blockObject;
     public GameObject roofObject;
+    public GameObject fanObject;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Generate floor
         Instantiate(floorObject, new Vector3(0, 0, 0), Quaternion.identity);
         
         for(int i = 0; i < 10; i++)
         {
             for(int j = 0; j < 10; j++)
             {
-                int rand = Random.Range(10, 15);
+                // Generate random height building
+                int rand = Random.Range(20, 35);
                 GameObject batiment = Instantiate(blockObject, new Vector3((i - 5) * 15f + 7.5f, rand / 2f, (j - 5) * 15f + 7.5f), Quaternion.identity);
                 batiment.transform.localScale = new Vector3(batiment.transform.localScale.x, rand, batiment.transform.localScale.z);
                 Instantiate(roofObject, new Vector3((i - 5) * 15f + 7.5f, rand+0.5f, (j - 5) * 15f + 7.5f), Quaternion.identity);
+                if (i == 0 && j == 0)
+                {
+                    // Set player height
+                } else
+                {
+                    // Set roof obstacles
+                    GameObject obstacle = Instantiate(fanObject, new Vector3((i - 5) * 15f + 7.5f, rand + 1f, (j - 5) * 15f + 7.5f), Quaternion.identity);
+                    int obstSize = Random.Range(1, 3);
+                    int obstPos = Random.Range(0, 3);
+                }
             }
         }
     }
